bfb907bf4d7b98661ba5f990bba86bb53ef991a9
felixxwu <felixxwu@gmail.com>
2019-07-29 13:05:16 +0100

    finished writing tests for collection.js

--- tests/unit/Collection.spec.js
+++ tests/unit/Collection.spec.js
@@ -26,24 +26,54 @@ describe('Collection.js', function() {
       }
     })
 
-    jvGetStub = sinon.stub().callsFake(() => {
+    jvGetStub = sinon.stub()
+    jvGetStub.withArgs('people').callsFake(() => {
       jvGetSpy()
       return {
         1: {
           first_name: 'Alice',
           last_name: 'Smith',
+          cats: {},
         },
         2: {
           first_name: 'Bob',
           last_name: 'Smithers',
+          cats: {
+            1: {
+              name: 'Ella',
+            },
+          },
         },
         3: {
           first_name: 'Charlie',
           last_name: 'Smithson',
+          cats: {
+            1: {
+              name: 'Ella',
+            },
+            2: {
+              name: 'Felix',
+            },
+          },
         },
         4: {
           first_name: 'David',
           last_name: 'Smith',
+          cats: {},
+        },
+      }
+    })
+    jvGetStub.withArgs('cats').callsFake(() => {
+      jvGetSpy()
+      return {
+        1: {
+          name: 'Ella',
+        },
+        2: {
+          name: 'Felix',
+        },
+        3: {
+          name: 'Gelard',
         },
       }
     })
@@ -62,11 +92,7 @@ describe('Collection.js', function() {
           searchOperator: 'matches',
         },
         {
-          name: 'cat.name',
-          alias: 'Cat',
-        },
-        {
-          name: 'dog.name',
+          name: 'cats.name',
         },
       ],
       previewOrder: ['last_name', 'first_name'],
@@ -89,14 +115,37 @@ describe('Collection.js', function() {
       1: {
         first_name: 'Alice',
         last_name: 'Smith',
+        cats: {},
       },
       2: {
         first_name: 'Bob',
         last_name: 'Smithers',
+        cats: {
+          1: {
+            _jv: {
+              type: 'cats',
+              id: 1,
+            },
+          },
+        },
       },
       3: {
         first_name: 'Charlie',
         last_name: 'Smithson',
+        cats: {
+          1: {
+            _jv: {
+              type: 'cats',
+              id: 1,
+            },
+          },
+          2: {
+            _jv: {
+              type: 'cats',
+              id: 2,
+            },
+          },
+        },
       },
     }
   })
@@ -106,7 +155,7 @@ describe('Collection.js', function() {
       name: 'people',
       localstore: localstore,
       globalstore: globalstore,
-      columnNames: ['first_name', 'last_name', 'cat.name', 'dog.name'],
+      columnNames: ['first_name', 'last_name', 'cats.name'],
       options: {
         name: 'people',
         columns: [
@@ -125,15 +174,8 @@ describe('Collection.js', function() {
             searchable: true,
           },
           {
-            name: 'cat.name',
-            alias: 'Cat',
-            caseSensitive: false,
-            searchOperator: 'contains',
-            searchable: true,
-          },
-          {
-            name: 'dog.name',
-            alias: 'dog.name',
+            name: 'cats.name',
+            alias: 'cats.name',
             caseSensitive: false,
             searchOperator: 'contains',
             searchable: true,
@@ -164,14 +206,13 @@ describe('Collection.js', function() {
   it('provides the alias for a column', function() {
     expect(collection.getAlias('first_name')).to.equal('First Name')
     expect(collection.getAlias('last_name')).to.equal('Surname')
-    expect(collection.getAlias('cat.name')).to.equal('Cat')
   })
 
   it('uses the column name as the alias if none is specified', function() {
-    expect(collection.getAlias('dog.name')).to.equal('dog.name')
+    expect(collection.getAlias('cats.name')).to.equal('cats.name')
   })
 
-  it('gets an entry from the search results and not store', function() {
+  it('gets an entry from the search results (and not store)', function() {
     expect(collection.get(1)).to.equal(collection.searchResults[1])
     expect(collection.get(2)).to.equal(collection.searchResults[2])
     expect(collection.get(3)).to.equal(collection.searchResults[3])
@@ -182,6 +223,7 @@ describe('Collection.js', function() {
     expect(collection.get(4)).to.deep.equal({
       first_name: 'David',
       last_name: 'Smith',
+      cats: {},
     })
     expect(jvGetSpy.callCount).to.equal(1)
   })
@@ -200,6 +242,7 @@ describe('Collection.js', function() {
     expect(collection.get(4, true)).to.deep.equal({
       first_name: 'David',
       last_name: 'Smith',
+      cats: {},
     })
     expect(jvGetSpy.callCount).to.equal(1)
   })
@@ -209,10 +252,19 @@ describe('Collection.js', function() {
       1: {
         first_name: 'Alice',
         last_name: 'Smith',
+        cats: {},
       },
       3: {
         first_name: 'Charlie',
         last_name: 'Smithson',
+        cats: {
+          1: {
+            name: 'Ella',
+          },
+          2: {
+            name: 'Felix',
+          },
+        },
       },
     })
   })
@@ -223,10 +275,19 @@ describe('Collection.js', function() {
       3: {
         first_name: 'Charlie',
         last_name: 'Smithson',
+        cats: {
+          1: {
+            name: 'Ella',
+          },
+          2: {
+            name: 'Felix',
+          },
+        },
       },
       4: {
         first_name: 'David',
         last_name: 'Smith',
+        cats: {},
       },
     })
   })
@@ -237,10 +298,12 @@ describe('Collection.js', function() {
       1: {
         first_name: 'Alice',
         last_name: 'Smith',
+        cats: {},
       },
       4: {
         first_name: 'David',
         last_name: 'Smith',
+        cats: {},
       },
     })
   })
@@ -262,5 +325,114 @@ describe('Collection.js', function() {
   it('returns no results for an empty seach', function() {
     // ignores Bob even though 'Smithers' has an 'i'
     expect(collection.filter('')).to.deep.equal({})
+  })
+
+  // the function ids() will determine in which order the results are displayed
+  it('returns a list of IDs sorted by ID', function() {
+    expect(collection.ids()).to.deep.equal(['1', '2', '3'])
+  })
+
+  it('returns a list of IDs sorted by a column', function() {
+    // load more interestingly sortable data
+    collection.searchResults = {
+      1: {
+        first_name: 'Bob',
+        last_name: 'Ross',
+      },
+      2: {
+        first_name: 'Alice',
+        last_name: 'Wonderland',
+      },
+      3: {
+        first_name: 'David',
+        last_name: 'Tennant',
+      },
+      4: {
+        first_name: 'Charlie',
+        last_name: 'Sheen',
+      },
+    }
+
+    collection.columnSorting = {
+      column: 1,
+      sortOrder: 'desc',
+    }
+    expect(collection.ids()).to.deep.equal(['3', '4', '1', '2'])
+
+    collection.columnSorting = {
+      column: 2,
+      sortOrder: 'asc',
+    }
+    expect(collection.ids()).to.deep.equal(['1', '4', '3', '2'])
+  })
+
+  it('returns the id and column name from a table coordinate', function() {
+    expect(collection.fromCoordinates(2, 0)).to.deep.equal({
+      id: '3',
+      col: 'first_name',
+    })
+    expect(collection.fromCoordinates(1, 1)).to.deep.equal({
+      id: '2',
+      col: 'last_name',
+    })
+  })
+
+  it('returns a table of data ready for handsontable to use', function() {
+    expect(collection.getAllData('details')).to.deep.equal([
+      ['details', 'Alice', 'Smith', '-'],
+      ['details', 'Bob', 'Smithers', 'Ella'],
+      ['details', 'Charlie', 'Smithson', '2 items...'],
+    ])
+  })
+
+  // tests for splitting data into two arrays for handsontable
+  it('returns all data in the top table with no expansion', function() {
+    // no row expanded
+    const splitTableData = collection.splitIntoTables({}, 'details')
+    expect(splitTableData.top).to.deep.equal([
+      ['details', 'Alice', 'Smith', '-'],
+      ['details', 'Bob', 'Smithers', 'Ella'],
+      ['details', 'Charlie', 'Smithson', '2 items...'],
+    ])
+    expect(splitTableData.topIDs).to.deep.equal(['1', '2', '3'])
+    expect(splitTableData.bottom).to.deep.equal([])
+    expect(splitTableData.bottomIDs).to.deep.equal([])
+  })
+
+  it('returns no data in top table if first is expanded', function() {
+    // row Alice expanded
+    const splitTableData = collection.splitIntoTables({ id: '1' }, 'details')
+    expect(splitTableData.top).to.deep.equal([])
+    expect(splitTableData.topIDs).to.deep.equal([])
+    expect(splitTableData.bottom).to.deep.equal([
+      ['details', 'Bob', 'Smithers', 'Ella'],
+      ['details', 'Charlie', 'Smithson', '2 items...'],
+    ])
+    expect(splitTableData.bottomIDs).to.deep.equal(['2', '3'])
+  })
+
+  it('returns data in both tables', function() {
+    // row Bob expanded
+    const splitTableData = collection.splitIntoTables({ id: '2' }, 'details')
+    expect(splitTableData.top).to.deep.equal([
+      ['details', 'Alice', 'Smith', '-'],
+    ])
+    expect(splitTableData.topIDs).to.deep.equal(['1'])
+    expect(splitTableData.bottom).to.deep.equal([
+      ['details', 'Charlie', 'Smithson', '2 items...'],
+    ])
+    expect(splitTableData.bottomIDs).to.deep.equal(['3'])
+  })
+
+  it('returns no data in bottom table if last is expanded', function() {
+    // row Bob expanded
+    const splitTableData = collection.splitIntoTables({ id: '3' }, 'details')
+    expect(splitTableData.top).to.deep.equal([
+      ['details', 'Alice', 'Smith', '-'],
+      ['details', 'Bob', 'Smithers', 'Ella'],
+    ])
+    expect(splitTableData.topIDs).to.deep.equal(['1', '2'])
+    expect(splitTableData.bottom).to.deep.equal([])
+    expect(splitTableData.bottomIDs).to.deep.equal([])
   })
 })
